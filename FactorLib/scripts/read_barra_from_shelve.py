e42561bb287efe171d52b1972eed4ca2f0e6a806
wanhanwan <wanshuai_shufe@163.com>
2017-09-07 15:45:26 +0800

    bug fix

--- FactorLib/scripts/read_barra_from_shelve.py
+++ FactorLib/scripts/read_barra_from_shelve.py
@@ -12,10 +12,10 @@ from contextlib import closing
 from FactorLib.data_source.base_data_source_h5 import h5
 
 fields = ['BETA', 'BLEV', 'BTOP', 'CETOP', 'CMRA', 'DASTD', 'DTOA',
-          'EGRLF', 'EGRO', 'EGRSF', 'EPFWD', 'ETOP', 'HSIGMA', 'LNCAP',
-          'MLEV', 'RSTR', 'SGRO', 'STOA', 'STOM', 'STOQ']
+          'EGRO', 'ETOP', 'HSIGMA', 'LNCAP', 'MLEV', 'RSTR', 'SGRO',
+          'STOA', 'STOM', 'STOQ']
 
-file_name = r"D:\data\barra\barra_%d"
+file_name = r"D:\data\barra\barra_%d"%2016
 
 with closing(shelve.open(file_name)) as f:
     for key in f.keys():
@@ -23,6 +23,5 @@ with closing(shelve.open(file_name)) as f:
             data = f[key]
             data.index = pd.DatetimeIndex(data.index, name='date')
             data = data.stack().to_frame(key).astype('float32')
-            data.index.names = ['date','IDs']
-        h5.save_factor(data, '/barra/')
-        
+            data.index.names = ['date', 'IDs']
+        h5.save_factor(data, '/barra/descriptors/')
