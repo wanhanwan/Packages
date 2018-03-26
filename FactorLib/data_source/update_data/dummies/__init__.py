2be9a5f05325de6498dcd62cc5489f2c860590c5
wanhanwan <wanshuai_shufe@163.com>
2018-03-26 13:46:20 +0800

    fix bugs

--- FactorLib/data_source/update_data/dummies/__init__.py
+++ FactorLib/data_source/update_data/dummies/__init__.py
@@ -1,10 +1,7 @@
-import pandas as pd
 from pathlib import Path
 
 
-def _read_dummy_class():
-    file_path = Path(__file__).parent.parent.parent / 'resource' / 'user_dummy.xlsx'
-    mapping = pd.ExcelFile()
-    return mapping
+file_path = Path(__file__).parent.parent.parent / 'resource' / 'user_dummy.xlsx'
+
 
 
--- FactorLib/data_source/update_data/dummies/class_1.py
+++ FactorLib/data_source/update_data/dummies/class_1.py
@@ -1,6 +1,11 @@
-from FactorLib.data_source.base_data_source_h5 import sec
+from FactorLib.data_source.base_data_source_h5 import sec, ncdb
+from . import file_path
+import pandas as pd
 
 
 def update(start_date, end_date):
+    mapping = pd.read_excel(file_path, header=0, sheet_name='class_1')
     indu_info = sec.get_stock_industry_info(None, '中信一级', start_date=start_date,
                                             end_date=end_date)
+    indu_info['tagID'] = indu_info['cs_level_1'].map(mapping)
+    ncdb.save_as_dummy()
