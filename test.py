57b8938bd810f14ccf722fb2e3f9a33f048874e6
wanhanwan <wanshuai_shufe@163.com>
2018-05-21 13:47:44 +0800

    修复了NeutralizeByRiskFactors函数Bug

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
 import pandas as pd
+from QuantLib.utils import NeutralizeByRiskFactors
 from FactorLib.data_source.base_data_source_h5 import data_source
-from FactorLib.data_source.ncdb import NCDB
-from FactorLib.data_source.h5db import H5DB
-from FactorLib.riskmodel.riskmodel_data_source import RiskDataSource
 
-nc = NCDB(r'D:\data\risk_model\xy')
-h5 = H5DB(r'D:\data\risk_model\xy')
-# data = nc.load_factor('risk_factor', '/factorData/')
-#
-# h5.save_h5file(data, '/factorData/', name='risk_factor')
 
-# dates = data_source.trade_calendar.get_trade_days('20100101', '20180630', '1m')
-# ds = RiskDataSource('xy')
-# data = ds.load_factors(factor_names=['Size'], dates=dates)
-
-h5.read_h5file_attr('risk_factor', '/factorData/', 'factors')
+dates = data_source.trade_calendar.get_trade_days('20180301', '20180331', '1m')
+data = data_source.load_factor("StyleFactor_VGS", "/XYData/StyleFactor/", dates=dates)
+data_neu = NeutralizeByRiskFactors(data, factor_name=data.columns[0])
