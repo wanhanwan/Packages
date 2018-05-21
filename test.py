5173c30b285df6538d859e1fbbb535d36288df09
wanhanwan <wanshuai_shufe@163.com>
2018-05-21 13:59:45 +0800

    udpate .gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from QuantLib.utils import NeutralizeByRiskFactors
-from FactorLib.data_source.base_data_source_h5 import data_source
-
-
-dates = data_source.trade_calendar.get_trade_days('20180301', '20180331', '1m')
-data = data_source.load_factor("StyleFactor_VGS", "/XYData/StyleFactor/", dates=dates)
-data_neu = NeutralizeByRiskFactors(data, factor_name=data.columns[0])
