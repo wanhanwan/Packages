4a480f316f1ea8bf31925e1962039aba615dc0c6
wanhanwan <wanshuai_shufe@163.com>
2019-10-21 15:27:22 +0800

    fix bugs

---
+++ QuantLib/portfolio/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+# author: wamhanwan
+from QuantLib.portfolio.nav_calculator import get_nav
+from QuantLib.portfolio.nav_analyser import Analyser as NavAnalyser
+from QuantLib.portfolio.plotting import plot_nav_year_by_year
